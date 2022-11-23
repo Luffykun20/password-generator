2134eff42595758a698ee8a2e14cc09db6e1fe7f
cristhian bustamante <cristhian_georgia_20@hotmail.com>
2022-11-23 00:11:19 -0500

    finishing challenge

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,48 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "`~@#$%^&*()_-+=";
 
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var finalpassword = ""
+  var passwordSize = prompt("how many characters do you want between 8-128 in your new password?");
+  if (passwordSize >= 8 && passwordSize <= 128) {
+    var confirmlowercase = confirm("do you want lowercase in your new password?");
+    var confirmuppercase = confirm("do you want uppercase in your new password?");
+    var confirmnumeric = confirm("do you want numeric characters in your new password?");
+    var confirmspecial = confirm("do you want special characters in your new password?");
+
+    for (i = 0; i < passwordSize; i++) {
+
+
+      if (confirmlowercase && finalpassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * 26)
+        finalpassword = finalpassword + lowercase[index]
+
+      }
+      if (confirmuppercase && finalpassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * 26)
+        finalpassword = finalpassword + uppercase[index]
+      }
+      if (confirmnumeric && finalpassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * 10)
+        finalpassword = finalpassword + numeric[index]
+      }
+      if (confirmspecial && finalpassword.length < passwordSize) {
+        var index = Math.floor(Math.random() * 15)
+        finalpassword = finalpassword + special[index]
+      }
+    }
+  }
+
+  else {
+    alert("choose password size between 8-128 characters")
+  }
+  return finalpassword
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
